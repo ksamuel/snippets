dc68b3558395b8f1bef2a3719eb90078c93f30bb
ksamuel <ksamuel@users.noreply.github.com>
2021-10-23 12:53:02 +0200

    Update sitecustomize.py

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,6 +1,8 @@
 import builtins
+from functools import partial
 from collections import *
-from prettyprint import pprint as pretty_print
+from pprint import pprint as pretty_print
+from inspect import getmembers, ismethod, stack
 
 STDLIB_COLLECTIONS = (
     str,
@@ -22,6 +24,16 @@ STDLIB_COLLECTIONS = (
     Counter,
 )
 
+def is_public_attribute(obj, name, methods=()):
+    return not name.startswith("_") and name not in methods and hasattr(obj, name)
+
+
+def attributes(obj):
+    members = getmembers(type(obj))
+    methods = {name for name, val in members if callable(val)}
+    is_allowed = partial(is_public_attribute, methods=methods)
+    return {name: getattr(obj, name) for name in dir(obj) if is_allowed(obj, name)}
+
 
 try:
     
@@ -30,36 +42,37 @@ try:
     from rich.traceback import install
     install()
     from rich.pretty import pprint
-    builtins.pp = builtins.pprint = pprint 
- 
+     
+    
+    class Printer(float):
+
+        def __call__(self, *args, **kwargs):
+            pprint(*args, **kwargs)
+        def __truediv__(self, other):
+            pprint(other)
+
+        def __rtruediv__(self, other):
+            pprint(other)
+            
+    builtins.pp = builtins.pprint = Printer()
+
 except ImportError:
-    def pprint(obj):
-        if isinstance(obj, STDLIB_COLLECTIONS):
-            pretty_print(obj)
-        else:
-            try:
-                name = "class " + obj.__name__
-            except AttributeError:
-                name = obj.__class__.__name__ + "()"
-            class_name = obj.__class__.__name__
-            print(name + ":")
-            attrs = attributes(obj)
-            if not attrs:
-                print("    <No attributes>")
-            for name, val in attributes(obj).items():
-                print("   ", name, "=", val)
+    pass
 
-
+def inspect(obj):
+    if isinstance(obj, STDLIB_COLLECTIONS):
+        pretty_print(obj)
+    else:
+        try:
+            name = "class " + obj.__name__
+        except AttributeError:
+            name = obj.__class__.__name__ + "()"
+        class_name = obj.__class__.__name__
+        print(name + ":")
+        attrs = attributes(obj)
+        if not attrs:
+            print("    <No attributes>")
+        for name, val in attributes(obj).items():
+            print("   ", name, "=", val)
  
-class Printer(float):
-
-    def __call__(self, *args, **kwargs):
-        pprint(*args, **kwargs)
-    def __truediv__(self, other):
-        pprint(other)
-
-    def __rtruediv__(self, other):
-        pprint(other)
-
-
-pp = Printer()
+builtins.inspect = inspect
